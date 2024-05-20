628edd775bad5dac026c63abd3ac579349d34770
firmin nagnduli <finganduli@gmail.com>
2024-05-20 14:00:41 +0200

    fix sdk dependances

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View
+      style={{
+        backgroundColor: "red",
+      }}
+    >
+      <Text
+        style={{
+          color: "white",
+        }}
+      >
+        Hello didjdkdkkjjfjfjj
+      </Text>
+    </View>
+  );
+}
