e256d267e6f8e883446b59f3b398180016407197
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-21 11:38:12 +0200

    fix issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return <Tabs />;
 }
+=======
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack />;
+}
+>>>>>>> 4c4d743 (fix same errors)
