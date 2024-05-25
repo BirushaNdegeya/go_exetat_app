d2f1a34276b3373e1da296c8215fc0f841908dce
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-25 20:32:40 +0200

    add page content

--- app/(tabs)/challenge.tsx
+++ app/(tabs)/challenge.tsx
@@ -1,12 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 
-export default function Challenge () {
+export default function Challenge() {
    const { container, text } = styles;
    return (
-      <View style={container}>
+      <ScrollView style={container}>
          <Text style={text}>EXPLORE SCREEN</Text>
-      </View>
+         <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non error omnis, reiciendis, deleniti ex recusandae accusantium, illo natus atque repellat molestiae expedita ipsum. Voluptatibus quia sit iste, assumenda architecto iusto!
+            In asperiores error sapiente nisi ab nemo! Culpa veritatis tempora rerum ullam doloribus ratione vero reiciendis cupiditate vitae minus odit, accusamus atque nemo aliquid, dicta dolores illo dolorum iure? Doloremque.
+            Nisi, suscipit itaque excepturi esse temporibus deleniti incidunt nesciunt veniam, maxime amet vero odio ea quas tempore facere placeat libero reiciendis quam, voluptatibus ad? Quibusdam, debitis voluptates. Aperiam, sunt tenetur.
+            Commodi aut reiciendis consectetur eveniet a asperiores eos omnis voluptatem. Harum ex optio vitae obcaecati odio fugit eaque, asperiores accusantium eos repellat sunt sit fuga temporibus corporis facere ipsa laudantium.
+            Deleniti quasi itaque sint ullam in velit culpa eaque. Quaerat laudantium tenetur molestias molestiae quasi assumenda mollitia in eius incidunt! Necessitatibus quis qui quos maiores vel pariatur excepturi optio ratione.
+            Saepe quam tenetur a odit, magnam rem atque inventore tempore alias in placeat dignissimos! Quasi impedit sunt aut tenetur ipsum iusto vero quos delectus quis! Enim inventore a illo molestiae.
+            Non, nulla delectus? Laudantium natus, odit possimus consequatur assumenda fugiat, dolores inventore explicabo esse similique nostrum est ad saepe sequi sint a velit, eum numquam! Itaque totam nam eaque voluptas.
+         </Text>
+      </ScrollView>
    );
 }
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 export default function Home () {
    const { container, text } = styles;
    return (
-      <View style={container}>
+      <ScrollView style={container}>
          <Text style={text}>EXPLORE SCREEN</Text>
-      </View>
+         <Ionicons />
+         <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam sint velit odit laudantium. Natus cumque numquam esse itaque provident voluptates? Voluptas omnis fugiat fuga necessitatibus cumque veritatis, perferendis adipisci nesciunt.
+            Quaerat aperiam voluptas quidem ea ratione sequi cumque expedita accusamus eaque nobis delectus adipisci excepturi, harum mollitia asperiores. Veritatis reiciendis eos nisi fuga aspernatur. Iusto qui dolores exercitationem consectetur voluptate.
+            Quidem velit harum sapiente, eveniet impedit iste corrupti voluptatem doloremque ducimus architecto, eius consequuntur facere consectetur sint esse, deserunt veritatis accusantium at. Voluptate deserunt eos rerum debitis non cumque suscipit!
+            Atque sed necessitatibus ducimus dignissimos recusandae nam modi earum eaque, tenetur, at, reprehenderit nesciunt. Laborum vel quisquam ducimus quam, dolore amet provident impedit nesciunt, quis ea dolorem nemo corrupti nobis.
+            Possimus soluta numquam recusandae dignissimos nostrum enim illo, quae, nemo velit consequuntur alias consectetur assumenda, amet nihil reiciendis laudantium qui eveniet dicta ad aliquam. Necessitatibus odio neque voluptatem dolorem sed.
+            Amet incidunt voluptatem rem temporibus libero minus excepturi ut sunt nam, consequatur id. Ducimus possimus necessitatibus qui culpa fugiat, quisquam illum repudiandae nobis consequatur, officia obcaecati beatae minima fugit ut!
+            Aut ducimus fugiat id rerum sunt dolorem, molestiae architecto inventore voluptas alias sapiente doloribus expedita tempora dolores voluptatibus dignissimos, nemo reprehenderit magnam accusantium cum? Iusto suscipit veniam quo consequatur totam!
+            Id ab laborum error reiciendis placeat quisquam iusto vel asperiores, quaerat ad. Porro quam aperiam qui expedita. Consequatur sunt hic, eos molestias asperiores qui repellendus neque placeat incidunt, illum optio!
+            Ipsum voluptatum illo ipsa iste, nisi facilis corporis quia, animi, reprehenderit sunt eveniet numquam culpa nam est tenetur cum ab ut reiciendis doloremque perferendis laboriosam ratione at voluptatem inventore. Quasi?
+            Facere ratione pariatur vero officiis laboriosam dolorem saepe tenetur quisquam temporibus culpa. Voluptas minima necessitatibus nobis? Nisi, modi debitis quisquam nemo quae reiciendis odit maxime, molestiae voluptas fuga fugiat. Iste!
+            Nisi natus fuga animi, eaque veritatis modi aperiam quod accusamus, culpa, corporis laborum similique mollitia quas facere rerum dolorum 
+         </Text>
+      </ScrollView>
    );
 }
 
